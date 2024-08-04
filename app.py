06f41b2bff986b85877a3840283a7fe67e7373d9
Manu Venugopal <manu.venugopal1994@gmail.com>
2024-08-03 22:19:16 -0400

    parking app.py commit

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-import requests
 import os
-import json
 from dotenv import load_dotenv
-from utils import find_closest_station
+from wmata_locator import WmataLocator
+import logging
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger()
 
 load_dotenv()
 
 API_KEY = os.getenv('WMATA_API_KEY')
 
-# response = requests.get(f'http://api.wmata.com/TrainPositions/TrainPositions?contentType=application/json&api_key={API_KEY}')
-
-response = requests.get(f'http://api.wmata.com/Rail.svc/json/jLines?contentType=application/json&api_key={API_KEY}')
-
-with open('line-information.json', 'w+') as f:
-    json.dump(response.json(), f, indent=4)
-        
+locator = WmataLocator(API_KEY, '607 13th St NW, Washington, DC 20005')
