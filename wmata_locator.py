67723c3009636923075b511e21e253f0f6d30e70
Manu Venugopal <manu.venugopal1994@gmail.com>
2024-08-03 22:18:51 -0400

    Add class for wmata locator

--- wmata_locator.py
+++ wmata_locator.py
@@ -1,16 +1,20 @@
 import json
 import time
 import logging
+import requests
 from geopy.distance import geodesic
-from geopy.geocoders import Nominatim
 from utils import get_coordinates_of_address
 
+logger = logging.getLogger()
+
 STATION_INFORMATION_FILEPATH = 'station-information.json'
+REAL_TIME_RAIL_PREDICTIONS_URL = 'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/{station_code}?contentType=application/json&api_key={api_key}'
 
 class WmataLocator:
     def __init__(self, api_key: str, current_address: str):
         self.api_key = api_key
         self.closest_station = WmataLocator.find_closest_station(current_address)
+        self.closest_station_name = self.closest_station["Name"]
 
     @staticmethod
     def find_closest_station(current_address: str):
@@ -35,8 +39,53 @@ class WmataLocator:
             if distance_from_station < closest_station_geodesic_distance:
                 closest_station_geodesic_distance = distance_from_station
                 closest_station = station
-        logger.info(f'Found closest station = {closest_station}')
+        logger.info(f'Found closest station = {closest_station["Name"]}')
         return closest_station
 
-def find_train_prediction_near_station(station_code):
-    train_positions = train_positions['TrainPositions']
+    def find_closest_train_prediction(self):
+        # get real time rail predictions
+        logging.info(f'Finding closest train predictions for {self.closest_station_name}...')
+        URL = REAL_TIME_RAIL_PREDICTIONS_URL.format_map({
+            'api_key': self.api_key,
+            'station_code': self.closest_station['Code']
+        })
+        response = requests.get(URL)
+        logging.info(f'Found closest train predictions for {self.closest_station_name}...')
+        trains = response.json()["Trains"]
+        line_map = {}
+        for train in trains:
+            line = train["Line"]
+            min = train["Min"]
+            destination = train["Destination"]
+
+            # convert "Min" ( minutes ) to something integer
+            time = None
+            if min.lower() == 'arr':
+                time = 0
+            else:
+                # try parsing min as integer
+                # if failed, just pass None
+                try:
+                    time = int(min)
+                except ValueError:
+                    pass
+
+            if line not in line_map:
+                line_map[line] = {
+                        destination: [time]
+                    }
+                
+            else:
+                if destination not in line_map[line]:
+                    line_map[line][destination] = [time]
+                else:
+                    line_map[line][destination].append(time)
+        
+        # now sort all the times for all the destinations
+
+        logger.info(f'Found lines {",".join(list(line_map.keys()))} for {self.closest_station_name}')
+        for line, destinations in line_map.items():
+            for dest, times in destinations.items():
+                line_map[line][dest] = sorted(line_map[line][dest], key=lambda x: (x is None, x))
+        
+        return line_map
