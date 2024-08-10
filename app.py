0a78c5ebbcdda5d273c911832ffd357dc76669c9
Manu Venugopal <manu.venugopal1994@gmail.com>
2024-08-10 01:19:30 -0400

    organize esp32 conversion script into utils

--- app.py
+++ app.py
@@ -4,34 +4,8 @@ from wmata_locator import WmataLocator
 from dotenv import load_dotenv
 import os
 import json
-from datetime import datetime
-import requests
 from requests import Request, Session
-def convert_for_esp32_led_matrix_64_32(train_predictions):
-  result = []
-  lines = train_predictions['line']
-  timestamp = train_predictions['timestamp']
-  timestamp = datetime.fromisoformat(timestamp).strftime('%I:%M:%S%p')
-  for line, dest_map in lines.items():
-    dest_list = []
-    for dest, times in dest_map.items():
-      dest_formatted = dest[:4]
-      dest_formatted = dest_formatted.replace(' ', '')
-      time_formatted = ','.join([str(t) for t in times[:2]])
-      time_formatted = time_formatted.rjust(5)
-      full_string = f'{dest_formatted}{time_formatted}'
-      if len(full_string) > 11:
-        raise Exception(f'The length of {full_string}({len(full_string)}) is greated than 16')
-      dest_list.append(f'{dest_formatted} {time_formatted}')
-    result.append({
-      'name': line,
-      'destinations': dest_list,
-    })
-  
-  return {
-    'line': result,
-    'timestamp': timestamp
-  }
+from utils import convert_for_esp32_led_matrix_64_32
 
   
 def main():
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import time
 import logging
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
+from datetime import datetime
 
 logger = logging.getLogger()
 
@@ -37,3 +38,30 @@ def get_coordinates_of_address(address):
     logger.info(f'Found coordinates for {address} = {coordinates}. Saving to cache {CACHE_FILE}...')
     save_cache(cache)
     return coordinates
+
+
+def convert_for_esp32_led_matrix_64_32(train_predictions):
+  result = []
+  lines = train_predictions['line']
+  timestamp = train_predictions['timestamp']
+  timestamp = datetime.fromisoformat(timestamp).strftime('%I:%M:%S%p')
+  for line, dest_map in lines.items():
+    dest_list = []
+    for dest, times in dest_map.items():
+      dest_formatted = dest[:4]
+      dest_formatted = dest_formatted.replace(' ', '')
+      time_formatted = ','.join([str(t) for t in times[:2]])
+      time_formatted = time_formatted.rjust(5)
+      full_string = f'{dest_formatted}{time_formatted}'
+      if len(full_string) > 11:
+        raise Exception(f'The length of {full_string}({len(full_string)}) is greated than 16')
+      dest_list.append(f'{dest_formatted} {time_formatted}')
+    result.append({
+      'name': line,
+      'destinations': dest_list,
+    })
+  
+  return {
+    'line': result,
+    'timestamp': timestamp
+  }
